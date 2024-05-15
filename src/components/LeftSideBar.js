73c8489250f4fe1a1c13f3230a2eeff2607a0af9
pritam khaplee <pritamkhaple8585@gmail.com>
2024-05-15 21:49:27 +0530

    com

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -15,7 +15,7 @@ function LeftSideBar() {
   return (
     <div className='w-[20%]'>
       <div className='ml-2'>
-        <img src="https://img.freepik.com/free-vector/twitter-new-2023-x-logo-white-background-vector_1017-45422.jpg?w=740&t=st=1715345345~exp=1715345945~hmac=691454d70da95bcec6d69886b756eed7b95dbb0b306882d650c95f62a2cdc369" alt='' width={"35px"}></img>
+        <img src="https://img.freepik.com/free-vector/twitter-new-2023-x-logo-white-background-vector_1017-45422.jpg?w=740&t=st=1715345345~exp=1715345945~hmac=691454d70da95bcec6d69886b756eed7b95dbb0b306882d650c95f62a2cdc369" alt='' width={"40px"}></img>
       </div>
       <div className='my-4'>
         <Link to="/" className='flex items-center  my-5 hover:bg-gray-200 rounded-full hover:cursor-pointer px-4 py-2'>
@@ -25,7 +25,7 @@ function LeftSideBar() {
         <IoIosOptions  size={"24px"} className='mr-5'/><h3 className='font-bold text-left'>Explore</h3>
         </div>
         <div className='flex items-center  my-5 hover:bg-gray-200 rounded-full hover:cursor-pointer px-4 py-2'>
-        <IoIosNotifications  size={"24px"} className='mr-5'/><h3 className='font-bold text-left'>Notification</h3>
+        <IoIosNotifications  size={"25px"} className='mr-5'/><h3 className='font-bold text-left'>Notification</h3>
         </div>
         <div className='flex items-center  my-5 hover:bg-gray-200 rounded-full hover:cursor-pointer px-4 py-2'>
         <FaMessage  size={"24px"} className='mr-5'/><h3 className='font-bold text-left'>Messages</h3>
